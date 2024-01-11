c9e506f270ba25a053964efa8fc5c1dff6b11192
Hit Shah <hit@icreativetechnologies.com>
2024-01-11 12:24:35 +0530

    Remove vendor folder

--- vendor/store.shopware.com/dtgsgoogletagmanagersw6/src/Resources/app/storefront/src/main.js
+++
@@ -1,8 +0,0 @@
-import DtgsGoogleTagManagerPlugin from './plugin/dtgs-google-tag-manager/dtgs-google-tag-manager.plugin';
-
-window.PluginManager.register('GoogleTagManager', DtgsGoogleTagManagerPlugin);
-
-// Necessary for the webpack hot module reloading server
-if (module.hot) {
-    module.hot.accept();
-}
--- vendor/store.shopware.com/kplngiproductorder/src/Resources/app/administration/src/module/extension/kplngi-category-view/index.js
+++
@@ -1,5 +0,0 @@
-import template from './kplngi-category-view.html.twig';
-
-Shopware.Component.override('sw-category-view', {
-    template
-});
